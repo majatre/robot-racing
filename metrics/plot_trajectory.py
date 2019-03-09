ad5bf42b20c223a2f68dde03425b3ab1f2c37f86
Maja <mt675@cam.ac.uk>
2019-03-09 09:52:18 +0000

    Metrics for evaluation

---
+++ metrics/plot_trajectory.py
@@ -0,0 +1,65 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pylab as plt
+# from rrt import OccupancyGrid
+from matplotlib.collections import LineCollection
+
+# def plot_race(occ_grid):
+#   data_path = np.genfromtxt('/tmp/gazebo_race_path.txt', delimiter=',')
+#   data_trajectory = np.genfromtxt('/tmp/gazebo_race_trajectory.txt', delimiter=',')
+
+#   plt.figure()
+#   plt.plot(data_path[:, 0], data_path[:, 1], 'b', label='path')
+#   plt.plot(data_trajectory[:, 0], data_trajectory[:, 1], 'g', label='true')
+
+#   occ_grid.draw()
+#   plt.show()
+
+
+# def plot_velocity(occ_grid):
+#   data_path = np.genfromtxt('/tmp/gazebo_race_path.txt', delimiter=',')
+#   data_trajectory = np.genfromtxt('/tmp/gazebo_race_trajectory.txt', delimiter=',')
+
+#   fig, axs = plt.subplots()
+#   velocities = data_trajectory[:, 2]
+#   # Create a continuous norm to map from data points to colors
+#   norm = plt.Normalize(velocities.min(), velocities.max())
+#   points = np.array([data_trajectory[:, 0], data_trajectory[:, 1]]).T.reshape(-1, 1, 2)
+#   segments = np.concatenate([points[:-1], points[1:]], axis=1)
+#   lc = LineCollection(segments, cmap='Greens', norm=norm)
+#   # Set the values used for colormapping
+#   lc.set_array(velocities)
+#   lc.set_linewidth(2)
+#   line = axs.add_collection(lc)
+#   plt.colorbar(line, ax=axs)
+
+#   occ_grid.draw()
+
+#   plt.show()
+
+
+def velocity_histogram(file_paths):
+  data = []
+  for fp in file_paths:
+    data_path = np.genfromtxt(fp, delimiter=',')
+    velocities = [x[2] for x in data_path]
+    data.append(velocities)
+  n, bins, patches = plt.hist(data, 25)
+  plt.show()
+
+
+def velocity_over_time(file_paths):
+  #data_path = np.genfromtxt(file_path, delimiter=',')
+  for fp in file_paths:
+    data_path = np.genfromtxt(fp, delimiter=',')
+    velocities = [x[2] for x in data_path]
+    plt.plot(range(len(velocities)), velocities)
+  plt.show()
+
+#velocity_histogram(['gazebo_race_trajectory.txt', 'wavefront_gazebo_race_trajectory.txt'])
+velocity_over_time(['gazebo_race_trajectory.txt', 'wavefront_gazebo_race_trajectory.txt'])
+
+
